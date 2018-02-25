3a516a112c3cea6638517a97e5222b9999880a91
Maks <materkov@gmail.com>
2018-02-25 19:48:43 +0300

    Added https everywhere

--- lego/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -v /apps/lego:/lego \
-  -e "AWS_REGION=eu-central-1" \
-  -e "AWS_ACCESS_KEY_ID=AKIAJPVPQJRPDLD46X3A" \
-  -e "AWS_SECRET_ACCESS_KEY=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" \
-  563473344515.dkr.ecr.eu-central-1.amazonaws.com/lego:1 \
-  --path /lego --accept-tos --email="materkov@gmail.com" --domains="mmaks.me" --dns="route53" run
--- lego/gen.sh
+++ lego/gen.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
-echo "tototo"
 
-AWS_REGION=$(curl http://consul-server:8500/v1/kv/lego/aws_credentials/region?raw)
-AWS_ACCESS_KEY_ID=$(curl http://consul-server:8500/v1/kv/lego/aws_credentials/ ?raw)
-AWS_SECRET_ACCESS_KEY=$(curl http://consul-server:8500/v1/kv/lego/aws_credentials/access_key_secret?raw)
+export AWS_REGION="$(curl http://consul-server:8500/v1/kv/lego/aws_credentials/region?raw)"
+export AWS_ACCESS_KEY_ID="$(curl http://consul-server:8500/v1/kv/lego/aws_credentials/access_key_id?raw)"
+export AWS_SECRET_ACCESS_KEY="$(curl http://consul-server:8500/v1/kv/lego/aws_credentials/access_key_secret?raw)"
+export AWS_HOSTED_ZONE_ID="$(curl http://consul-server:8500/v1/kv/lego/aws_credentials/zone_id?raw)"
 
-lego --path /lego --accept-tos --email="materkov@gmail.com" --domains="mmaks.me" --dns="route53" run
-  
+lego --path /lego --accept-tos --email="materkov@gmail.com" --dns="route53" \
+  --domains="mmaks.me" --domains="consul.mmaks.me" \
+  run
