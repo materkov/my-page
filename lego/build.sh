ef842c3c777c85f8b7c29270c65569b41e633e30
Maks <materkov@gmail.com>
2018-03-06 01:51:26 +0300

    Fixed lego container

---
+++ lego/build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker build -t 563473344515.dkr.ecr.eu-central-1.amazonaws.com/lego:latest .
+docker push 563473344515.dkr.ecr.eu-central-1.amazonaws.com/lego:latest
--- lego/gen.sh
+++ lego/gen.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env sh
 
-export AWS_REGION="$(curl http://consul-server:8500/v1/kv/lego/aws_credentials/region?raw)"
-export AWS_ACCESS_KEY_ID="$(curl http://consul-server:8500/v1/kv/lego/aws_credentials/access_key_id?raw)"
-export AWS_SECRET_ACCESS_KEY="$(curl http://consul-server:8500/v1/kv/lego/aws_credentials/access_key_secret?raw)"
-export AWS_HOSTED_ZONE_ID="$(curl http://consul-server:8500/v1/kv/lego/aws_credentials/zone_id?raw)"
-
-lego --path /lego --accept-tos --email="materkov@gmail.com" --dns="route53" \
+docker run -v /apps/lego:/apps/lego --env-file=../.lego.env --rm \
+  563473344515.dkr.ecr.eu-central-1.amazonaws.com/lego:latest \
+  --path /apps/lego --accept-tos --email="materkov@gmail.com" --dns="route53" \
   --domains="mmaks.me" --domains="consul.mmaks.me" --domains="twitch2telegram.mmaks.me" \
   run
